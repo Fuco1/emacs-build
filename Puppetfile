43c35eb2e2b6590ff3c46c205e242d37a114b739
Matus Goljer <matus.goljer@gmail.com>
2017-06-05 22:26:02 +0200

    Use librarian-puppet to install puppet dependencies

---
+++ Puppetfile
@@ -0,0 +1,7 @@
+forge "https://forgeapi.puppetlabs.com"
+
+mod "puppetlabs/stdlib", '4.15.0'
+
+# Local Variables:
+# mode: Ruby
+# End:
--- vagrant/puppet.sh
+++ vagrant/puppet.sh
@@ -3,21 +3,26 @@ set -e
 
 if which puppet > /dev/null; then
   echo "Puppet is already installed."
-  exit 0
+else
+  # Do the initial apt-get update
+  echo "Initial apt-get update..."
+  apt-get update >/dev/null
+
+  # Install Puppet
+  echo "Installing Puppet..."
+  apt-get --yes install puppet >/dev/null
+
+  echo "Puppet installed!"
 fi
 
-# Do the initial apt-get update
-echo "Initial apt-get update..."
-apt-get update >/dev/null
-
-# Install Puppet
-echo "Installing Puppet..."
-apt-get --yes install puppet >/dev/null
-
-echo "Puppet installed!"
-
 echo "Installing RubyGems..."
-apt-get --yes install rubygems >/dev/null
+apt-get --yes install rubygems ruby-bundler >/dev/null
 
 gem install --no-ri --no-rdoc rubygems-update
 update_rubygems >/dev/null
+
+echo "bundle install..."
+bundle install --path vendor/bundle
+
+echo "librarian-puppet install..."
+bundle exec librarian-puppet install
