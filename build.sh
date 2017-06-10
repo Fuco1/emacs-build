7d33cff25bc01fe748c68647121fc5ae8ea93581
Matus Goljer <matus.goljer@gmail.com>
2017-06-10 15:43:52 +0200

    run make twice only on error

--- build.sh
+++ build.sh
@@ -25,8 +25,7 @@ fi
 cd "$PWD/emacs-$VERSION"
 ./configure --with-x-toolkit=no
 make clean
-make -j 2
 # We make twice because the first time this error happens
 #   Adding new name: Operation not permitted, /vagrant/emacs-25.2/src/emacs, /vagrant/emacs-25.2/src/emacs-25.2.1
 # /shrug
-make -j 2
+make -j 2 || make -j 2
